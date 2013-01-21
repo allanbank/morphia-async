451c25d66542c056f410bc04505c10e01b94f890
Robert J. Moore <Robert.J.Moore@allanbank.com>
2013-01-21 13:32:22 -0500

    Updates to get the morphia-entityscanner to compile with the async driver.

--- entityscanner-plug/src/test/java/com/google/code/morphia/ext/entityscanner/E.java
+++ entityscanner-plug/src/test/java/com/google/code/morphia/ext/entityscanner/E.java
@@ -3,8 +3,7 @@
  */
 package com.google.code.morphia.ext.entityscanner;
 
-import org.bson.types.ObjectId;
-
+import com.allanbank.mongodb.bson.element.ObjectId;
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
 
